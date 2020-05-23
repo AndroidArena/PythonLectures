a4868c2df951d56ad5b889218199ca3f2e409e67
Dinesh Raturi <dineshraturi22@gmail.com>
2020-05-23 21:33:48 +0530

    Add files via upload

---
+++ callMyModule.py
@@ -0,0 +1,16 @@
+import testModule
+
+#print(dir(testModule))
+#print(help(testModule))
+#print(testModule.add.__doc__)
+
+
+#Pydoc commands
+#1.python -m pydoc
+#2.python -m pydoc math
+#3.python -m pydoc testModule
+#4.python -m pydoc -k url 
+#5.python -m pydoc -p 314   #Bydeafault 0 means any unused port will be consider
+#6. # save document to local file using -w
+#python -m pydoc -w testModule
+#start testModule.html
---
+++ testModule.py
@@ -0,0 +1,36 @@
+'''
+This Module is 
+used to nake life easy for a user'''
+
+def add(num1,num2):
+    '''This function is 
+         used to Add Two numbers''' 
+    return(num1+num2)
+
+
+def multiply(num1,num2):
+    '''This function is 
+         used to Multiply Two numbers''' 
+    return(num1*num2)   
+    
+def greeting(name):
+    '''
+    This function is 
+    used to greet user'''
+    print("Hello, " + name)
+    
+def calling ():
+    '''This function is 
+         used to call all the''' 
+    print(add(num1,num2))
+    print(multiply(num1,num2))
+    print(greeting())
+  
+num1=100
+num2=200
+
+if __name__ == "main" :
+    calling()
+
+__author__ = "Dinesh"
+__copyright__ = "Copyright Apache licence 2020"
